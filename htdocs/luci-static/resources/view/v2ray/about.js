07bcab991c93b270ea8f460ecbd7a50a97c90139
Frand Ren <coflery@gmail.com>
2025-10-19 22:41:26 +0800

    chore: init project for luci2

---
+++ htdocs/luci-static/resources/view/v2ray/about.js
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * Copyright 2020 Xingwang Liao <kuoruan@gmail.com>
+ *
+ * Licensed to the public under the MIT License.
+ */
+"use strict";"require fs";"require uci";"require ui";return L.view.extend({load:function(){return uci.load("v2ray").then((function(){var e=uci.get("v2ray","main","config_file");return e||(e="/var/etc/v2ray/v2ray.main.json"),Promise.all([Promise.resolve(e),L.resolveDefault(fs.read(e),"")])}))},render:function(e){var r=void 0===e?[]:e,a=r[0],t=void 0===a?"":a,o=r[1],s=void 0===o?"":o;return E([E("h2","%s - %s".format(_("V2Ray"),_("About"))),E("p",_("LuCI support for V2Ray.")),E("p",_("Version: %s").format("2.0.0"+"-"+"1")),E("p",_("Author: %s").format("Xingwang Liao")),E("p",_("Source: %s").format('<a href="https://github.com/coflery/luci-app-v2ray" target="_blank">https://github.com/coflery/luci-app-v2ray</a>')),E("p",_("Latest: %s").format('<a href="https://github.com/coflery/luci-app-v2ray/releases/latest" target="_blank">https://github.com/coflery/luci-app-v2ray/releases/latest</a>')),E("p",_("Report Bugs: %s").format('<a href="https://github.com/coflery/luci-app-v2ray/issues" target="_blank">https://github.com/coflery/luci-app-v2ray/issues</a>')),E("p",_("Current Config File: %s").format(t)),E("pre",{style:"-moz-tab-size: 4;-o-tab-size: 4;tab-size: 4;word-break: break-all;"},s||_("Failed to open file."))])},handleReset:null,handleSave:null,handleSaveApply:null});
