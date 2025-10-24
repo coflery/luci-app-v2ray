7b697ec4c6e1f61643290a86d60f5dae4e3d73a4
Frand Ren <coflery@gmail.com>
2025-10-25 03:33:46 +0800

    feat: grpc transports support

--- src/view/v2ray/outbound.ts
+++ src/view/v2ray/outbound.ts
@@ -677,10 +677,10 @@ return L.view.extend<string[]>({
     o.value("http", "HTTP/2");
     o.value("domainsocket", "Domain Socket");
     o.value("quic", "QUIC");
+    o.value("grpc", "gRPC");
 
     o = s.taboption("stream", form.ListValue, "ss_security", _("Security"));
     o.modalonly = true;
-    o.value("");
     o.value("none", _("None"));
     o.value("tls", "TLS");
 
@@ -1044,6 +1044,63 @@ return L.view.extend<string[]>({
     o.value("wechat-video", _("Wechat Video"));
     o.value("dtls", "DTLS 1.2");
     o.value("wireguard", "WireGuard");
+
+    // Stream Settings - gRPC
+    o = s.taboption(
+      "stream",
+      form.Value,
+      "service_name",
+      "%s - %s".format("gRPC", _("Service name"))
+    );
+    o.depends("ss_network", "grpc");
+    o.modalonly = true;
+    o = s.taboption(
+      "stream",
+      form.Flag,
+      "multi_mode",
+      "%s - %s".format("gRPC", _("Multi mode"))
+    );
+    o.modalonly = true;
+    o.depends("ss_network", "grpc");
+
+    o = s.taboption(
+      "stream",
+      form.Value,
+      "idle_timeout",
+      "%s - %s".format("gRPC", _("Idle timeout"))
+    );
+    o.modalonly = true;
+    o.datatype = "uinteger";
+    o.depends("ss_network", "grpc");
+
+    o = s.taboption(
+      "stream",
+      form.Value,
+      "health_check_timeout",
+      "%s - %s".format("gRPC", _("Health check timeout"))
+    );
+    o.modalonly = true;
+    o.datatype = "uinteger";
+    o.depends("ss_network", "grpc");
+
+    o = s.taboption(
+      "stream",
+      form.Flag,
+      "permit_without_stream",
+      "%s - %s".format("gRPC", _("Permit without stream"))
+    );
+    o.modalonly = true;
+    o.depends("ss_network", "grpc");
+
+    o = s.taboption(
+      "stream",
+      form.Value,
+      "initial_windows_size",
+      "%s - %s".format("gRPC", _("Initial windows size"))
+    );
+    o.modalonly = true;
+    o.datatype = "uinteger";
+    o.depends("ss_network", "grpc");
 
     // Stream Settings - Socket Options
     o = s.taboption(
